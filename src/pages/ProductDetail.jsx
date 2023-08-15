5f586edd3749f949b24a701c47d2e061417acf20
Shwetaaaa2899 <shweta876jha@gmail.com>
2023-08-15 18:14:48 +0530

    bug fixed

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -21,7 +21,7 @@ const ProductDetail = () =>{
    
      const WishListHandler = (product) =>{
       // console.log("my token is")
-     if( token === null || !isloggedIn){
+     if( token === null ){
       toast("Please login first to shop your fav products")
    navigate("/auth")
    }
